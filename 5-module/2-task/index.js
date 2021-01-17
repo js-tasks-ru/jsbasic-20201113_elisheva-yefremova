5172b945fcb8dc461fa68c0aa89f4ff7f99bbd91
Yelizaveta Yefremova <elisheva.yefremova@gmail.com>
2021-01-17 22:19:47 +0200

    add 5-module 2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  document.addEventListener('click', function(e){
+    
+    if ( e.target.className == 'toggle-text-button' ) {
+      (text.hidden == false) ? text.hidden = true : text.hidden = false
+    }
+
+  })
 }
