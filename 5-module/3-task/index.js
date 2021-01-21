654d18acb0097e25f53b4db43ec6592a7715af93
Yelizaveta Yefremova <elisheva.yefremova@gmail.com>
2021-01-21 22:05:50 +0200

    add 5-module carousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,54 @@
 function initCarousel() {
-  // ваш код...
-}
+
+    let slides = 4,
+        position = 0,
+        currentSlide = 0,
+        slideWidth = document.querySelector('.carousel__slide').offsetWidth;
+  
+        
+    let track = document.querySelector('.carousel__inner'),
+        buttonPrev = document.querySelector('.carousel__arrow_left'),
+        buttonNext = document.querySelector('.carousel__arrow_right');
+  
+    function hideButton(elem) {
+      elem.style.display = 'none';
+    }
+  
+    function showButton(elem) {
+
+      elem.style.display = 'flex';
+
+    }
+  
+    function moveTrack(direction, currentSlide) {   
+      
+      track.style.transform = `translateX(-${ slideWidth * currentSlide }px)`;
+  
+      if ( currentSlide < 3 && currentSlide > 0 ) {
+        showButton(buttonPrev);
+        showButton(buttonNext)
+      } else if ( currentSlide == 3 )  {
+        hideButton(buttonNext); 
+      } else if ( currentSlide == 0 ) {
+          hideButton(buttonPrev);
+        }
+      
+    }
+  
+    hideButton(buttonPrev);
+  
+    buttonNext.addEventListener('click', function() {
+      
+      currentSlide++;
+      moveTrack('next', currentSlide);
+  
+    })
+  
+    buttonPrev.addEventListener('click', function() {
+    
+      currentSlide--;
+      moveTrack('prev', currentSlide)
+  
+    })
+      
+  }
